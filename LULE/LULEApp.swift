4a8fdd1eb718c3e49a70d3d1209177e382dea07b
Burak <burakduruk52@gmail.com>
2024-01-28 00:15:48 -0500

    Initial Commit

---
+++ LULE/LULEApp.swift
@@ -0,0 +1,17 @@
+//
+//  LULEApp.swift
+//  LULE
+//
+//  Created by Burak Duruk on 2024-01-28.
+//
+
+import SwiftUI
+
+@main
+struct LULEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
